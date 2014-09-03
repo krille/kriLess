18801092d97422a4ac6908df625529d194d0d898
Krille <kristofer.westling@gmail.com>
2014-09-03 10:27:08 +0200

    Spaces to tabs

--- gruntfile.js
+++ gruntfile.js
@@ -3,48 +3,59 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    less: {
-      main: {
-        options: {
-          paths: ['dev/css'],
-          compress: false,
-          sourceMap: true,
-          sourceMapFilename: 'dev/css/dev.css.map',
-          sourceMapBasepath: 'dev/css/'
-        },
-        files: {
-          'dev/css/dev.css': [
-            'dev/css/project.less'
-          ]
-        }
-      },
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie >= 8'],
-        map: true
-      },
-      single_file: {
-        src: 'dev/css/dev.css'
-      },
-    },
-    watch: {
-      less: {
-        files: ['dev/css/**/*.less', 'src/**/*.less'],
-        tasks: ['less', 'autoprefixer'],
-        options: {
-          spawn: false
-        }
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-autoprefixer');
+		pkg: grunt.file.readJSON('package.json'),
 
-  grunt.registerTask('default', ['less', 'autoprefixer']);
+		less: {
+			main: {
+				options: {
+					paths: ['dev/css'],
+					compress: false,
+					sourceMap: true,
+					sourceMapFilename: 'dev/css/dev.css.map',
+					sourceMapBasepath: 'dev/css/'
+				},
+				files: {
+					'dev/css/dev.css': [
+						'dev/css/project.less'
+					]
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: [
+					'last 2 versions', 
+					'ie >= 8'
+				],
+				map: true
+			},
+			single_file: {
+				src: 'dev/css/dev.css'
+			}
+		},
+		watch: {
+			less: {
+				files: [
+					'dev/css/**/*.less', 
+					'src/**/*.less'
+				],
+				tasks: [
+					'less', 
+					'autoprefixer'
+				],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+
+	grunt.registerTask('default', ['less', 'autoprefixer']);
 };
